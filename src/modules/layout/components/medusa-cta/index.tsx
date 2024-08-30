0d6c4e69658d93468c055699b5358dde9b3c7b75
nowayticket <gypsy8641@gmail.com>
2024-08-30 18:17:05 +0300

    Update index.tsx

--- src/modules/layout/components/medusa-cta/index.tsx
+++ src/modules/layout/components/medusa-cta/index.tsx
@@ -7,11 +7,7 @@ const MedusaCTA = () => {
   return (
     <Text className="flex gap-x-2 txt-compact-small-plus items-center">
       Powered by
-      <a href="https://www.medusajs.com" target="_blank" rel="noreferrer">
-        <Medusa fill="#9ca3af" className="fill-[#9ca3af]" />
-      </a>
-      &
-      <a href="https://nextjs.org" target="_blank" rel="noreferrer">
+      <a href="" target="_blank" rel="noreferrer">
         <NextJs fill="#9ca3af" />
       </a>
     </Text>
